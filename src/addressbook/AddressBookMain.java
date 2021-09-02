d589a11049206524c5a2f4992e0d7498f34c1eae
madan <madan@local.com>
2021-09-02 21:02:37 +0530

    added the code to input the address details from the user

--- src/addressbook/AddressBookMain.java
+++ src/addressbook/AddressBookMain.java
@@ -1,8 +1,29 @@
 package addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 	
 	public static void main(String[] args) {
 		System.out.println("welcome to address book program");
+		
+		String firstName;
+		String LastName;
+		String city;
+		String state;
+		String zipcode;
+		String phoneNumber;
+		String email;
+		
+		Scanner sc = new Scanner(System.in);
+		firstName = sc.nextLine();
+		LastName = sc.nextLine();
+		city = sc.nextLine();
+		state = sc.nextLine();
+		state = sc.nextLine();
+		zipcode = sc.nextLine();
+		phoneNumber = sc.nextLine();
+		email = sc.nextLine();
+		
 	}
 }
